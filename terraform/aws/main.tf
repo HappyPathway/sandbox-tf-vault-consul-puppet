d302d4f505ffec2c08bfb4952d991b9500bf9ab0
Nathan R Valentine <nrvale00@gmail.com>
2018-10-09 13:11:22 -0700

    actually...the Vault module also provisions the required Consul cluster. :D

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -17,16 +17,16 @@ variable "vault_cluster_name" {
   default = "vault-puppet-demo"
 }
 
-module "consul" {
-  source = "git::https://github.com/hashicorp/consul-guides//operations/provision-consul/quick-start/terraform-aws"
-  name = "${var.consul_cluster_name}"
-  consul_tags = "${var.tags}"
-  network_tags = "${var.tags}"
-}
-
-# module "vault" {
-#   source = "git::https://github.com/hashicorp/vault-guides//operations/provision-vault/quick-start/terraform-aws"
-#   name = "${var.vault_cluster_name}"
-#   vault_tags = "${var.tags}"
+# module "consul" {
+#   source = "git::https://github.com/hashicorp/consul-guides//operations/provision-consul/quick-start/terraform-aws"
+#   name = "${var.consul_cluster_name}"
+#   consul_tags = "${var.tags}"
 #   network_tags = "${var.tags}"
 # }
+
+module "vault" {
+  source = "git::https://github.com/hashicorp/vault-guides//operations/provision-vault/quick-start/terraform-aws"
+  name = "${var.vault_cluster_name}"
+  vault_tags = "${var.tags}"
+  network_tags = "${var.tags}"
+}
