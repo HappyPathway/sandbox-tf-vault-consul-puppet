2bd2a86b8ea9ed22c9cd4369bcb324e783ea555e
Nathan R Valentine <nrvale00@gmail.com>
2018-10-09 12:00:27 -0700

    we'll need these modules for a decent Consul and Vault setup

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,2 +1,32 @@
 provider "aws" {
 }
+
+variable "tags" {
+  type = "map"
+  default = {
+    owner = "nvalentine"
+    ttl = -1
+  }
+}
+
+variable "consul_cluster_name" {
+  default = "vault-puppet-demo"
+}
+
+variable "vault_cluster_name" {
+  default = "vault-puppet-demo"
+}
+
+module "consul" {
+  source = "git::https://github.com/hashicorp/consul-guides//operations/provision-consul/quick-start/terraform-aws"
+  name = "${var.consul_cluster_name}"
+  consul_tags = "${var.tags}"
+  network_tags = "${var.tags}"
+}
+
+module "vault" {
+  source = "git::https://github.com/hashicorp/vault-guides//operations/provision-vault/quick-start/terraform-aws"
+  name = "${var.vault_cluster_name}"
+  vault_tags = "${var.tags}"
+  network_tags = "${var.tags}"
+}
