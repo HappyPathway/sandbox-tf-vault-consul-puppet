4204e4270876588d6569f5f8c5b3cc582201e023
Nathan R Valentine <nrvale00@gmail.com>
2018-10-10 14:36:09 -0700

    add cloudinit bootstrap script for pe master

--- terraform/aws/puppet_server.tf
+++ terraform/aws/puppet_server.tf
@@ -14,29 +14,24 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-data "template_file" "pe_master_bootstrap" {
-  template = "${file("${path.module}/templates/pe_master_bootstrap.sh.tpl")}"
-
-  vars {
-  }
-}
-
 data "template_cloudinit_config" "pe_master_cloudinit" {
-  gzip = true
-  base64_encode = true
+  # gzipped user-data causes problems for Puppet default encoding JSON v PSON
+  gzip          = false
+  # it appears as though base64-encoded might also be causing problems
+  base64_encode = false
 
   part {
-    filename = "bootstrap.sh"
+    filename     = "bootstrap.sh"
     content_type = "text/x-shellscript"
-    content = "${data.template_file.pe_master_bootstrap.rendered}"
+    content      = "${file("${path.module}/templates/pe_master_bootstrap.sh.tpl")}"
   }
 }
 
 resource "aws_instance" "puppet-master" {
-  ami = "${data.aws_ami.ubuntu.id}"
+  ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "m1.large"
-  tags = "${merge(var.instance_tags, map("Name", "${var.vault_cluster_name}-puppet-master"))}"
-  subnet_id = "${element(module.vault.subnet_public_ids, 0)}"
-  user_data = "${data.template_cloudinit_config.pe_master_cloudinit.rendered}"
-  key_name = "${module.vault.ssh_key_name}"
+  tags          = "${merge(var.instance_tags, map("Name", "${var.vault_cluster_name}-puppet-master"))}"
+  subnet_id     = "${element(module.vault.subnet_public_ids, 0)}"
+  user_data     = "${data.template_cloudinit_config.pe_master_cloudinit.rendered}"
+  key_name      = "${module.vault.ssh_key_name}"
 }
--- terraform/aws/templates/pe_master_bootstrap.sh.tpl
+++ terraform/aws/templates/pe_master_bootstrap.sh.tpl
@@ -1,8 +1,26 @@
 #!/bin/bash
 
-set -eux
+: ${PE_DOWNLOAD_URI='https://pm.puppet.com/cgi-bin/download.cgi?arch=amd64&dist=ubuntu&rel=18.04&ver=latest'}
 
-msg = 'Hello from the PE Master bootstrap script!'
-
+msg='Hello from the PE Master bootstrap script!'
 logger $msg
 echo $msg
+
+set -eux
+
+wget -q -c -O /tmp/pe.tar.gz "${PE_DOWNLOAD_URI}"
+mkdir -p /tmp/pe
+tar xvf /tmp/pe.tar.gz -C /tmp/pe --strip-components 1
+apt-get update
+apt-get install -y ruby-hocon
+hocon -f /tmp/pe/conf.d/pe.conf set console_admin_password admin
+/tmp/pe/puppet-enterprise-installer -c /tmp/pe/conf.d/pe.conf -y
+
+sleep 10
+while true; do
+    set +e
+    puppet agent -t
+    if [ "$?" -eq "0" ]; then
+	set -e; break
+    fi
+done
