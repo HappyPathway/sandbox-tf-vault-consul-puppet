eb8a14f252d52d1ccf34e417db6a2525edd5fa9d
Nathan R Valentine <nrvale00@gmail.com>
2018-10-15 11:18:19 -0700

    add some addtl PE installer tunables + add Papertrail

--- terraform/scripts/pemaster_bootstrap.sh
+++ terraform/scripts/pemaster_bootstrap.sh
@@ -26,7 +26,14 @@ function pe_install() {
     public_hostname="$(facter ec2_metadata.public-hostname)"
 
     hocon -f /tmp/pe/conf.d/pe.conf set console_admin_password vault-puppet-demo
+
+    # There are a number of settings which have to be adjust to ensure proper operation though
+    # an AWS loadbalancer.
+    hocon -f /tmp/pe/conf.d/pe.conf set pe_install\"::\"master_pool_address "https\"://\"${public_hostname}\":\"8140"
     hocon -f /tmp/pe/conf.d/pe.conf set pe_install\"::\"puppet_master_dnsaltnames "[ \"${public_hostname}\" ]"
+    hocon -f /tmp/pe/conf.d/pe.conf set pe_repo\"::\"master "https\"://\"${public_hostname}\":\"8140"
+    hocon -f /tmp/pe/conf.d/pe.conf set puppet_enterprise\"::\"profile\"::\"agent\"::\"master_uris "[ \"https://${public_hostname}:8140\" ]"
+    hocon -f /tmp/pe/conf.d/pe.conf set puppet_enterprise\"::\"profile\"::\"agent\"::\"pcp_broker_list "[ \"https://${public_hostname}:8140\" ]"
 
     /tmp/pe/puppet-enterprise-installer -c /tmp/pe/conf.d/pe.conf -y
 
@@ -42,8 +49,18 @@ function pe_install() {
 
 
 function papertrail_install() {
-    wget -qO - --header='X-Papertrail-Token: ${papertrail_token}' \
-	 https://papertrailapp.com/destinations/10987402/setup.sh | bash
+    echo 'Installing Papertrail agent...'
+    wget -O /tmp/papertrail_setup.sh --header="X-Papertrail-Token: ${papertrail_token}" https://papertrailapp.com/destinations/10987402/setup.sh
+    chmod +x /tmp/papertrail_setup.sh
+    /tmp/papertrail_setup.sh -q
+    rm /tmp/papertrail_setup.sh
+}
+
+
+function goodbye() {
+    msg = 'PE Master bootstrap script finished.'
+    logger $msg
+    echo $msg
 }
 
 
@@ -56,6 +73,8 @@ main() {
 
     set -x
     pe_install
+    goodbye
+    exit 0
 }
 
 
