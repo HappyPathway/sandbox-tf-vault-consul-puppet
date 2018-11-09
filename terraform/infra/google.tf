3d6c9bc0ca5c71f2ea163ebfe0163ea0222de05e
Nathan R Valentine <nrvale00@gmail.com>
2018-11-09 10:43:07 -0800

    re-add Google infra

--- terraform/infra/google.tf
+++ terraform/infra/google.tf
@@ -1,11 +1,11 @@
-# module "infra_google" {
-#   source = "./modules/infra_google"
-#   gcp_credentials = "${var.gcp_credentials}"
-#   gcp_project = "${var.gcp_project}"
-#   instance_name = "gcp-${var.infra_prefix}"
-#   prefix = "${var.infra_prefix}"
-#   tags = "${var.infra_tags["project"]}"
-#   papertrail_token = "${var.papertrail_token}"
-#   consul_server = "${module.infra_aws.consul_server_address_public}"
-# }
-
+module "infra_google" {
+  source = "./modules/infra_google"
+  gcp_credentials = "${var.gcp_credentials}"
+  gcp_project = "${var.gcp_project}"
+  instance_name = "gcp-${var.infra_prefix}"
+  prefix = "${var.infra_prefix}"
+  tags = "${var.infra_tags["project"]}"
+  papertrail_token = "${var.papertrail_token}"
+  puppet_master_addr = "${module.infra_aws.puppet_master_address_public}"
+  ssh_key = "${module.infra_aws.puppet_ssh_public_key}"
+}
